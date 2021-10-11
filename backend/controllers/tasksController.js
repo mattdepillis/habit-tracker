1758d0636074cbd6253166b47804d17d3e2a09e9
mattdepillis <mattdepillis@gmail.com>
2021-10-11 18:44:32 -0400

    refactored getQuery for modularity and reordered functions in tasksController

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -1,17 +1,19 @@
 const config = require('../knexfile').development
 const knex = require('knex')(config)
 
+const getQueryBase = `
+  select t.task_id, task_description, task_deadline, task_status, task_type, task_priority,
+  task_product_manager, task_name, et.engineer_id, engineer_tasks_id, tt.tag_id, tagged_tasks_id,
+  engineer_name, e.label_color as engineer_label_color, tag_name, tg.label_color as tag_label_color
+  from tasks as t
+  left join engineer_tasks as et on t.task_id = et.task_id
+  left join tagged_tasks as tt on t.task_id = tt.task_id
+  left join engineer as e on et.engineer_id = e.engineer_id
+  left join tag as tg on tt.tag_id = tg.tag_id
+`
+
 const getTasks = (req, res) => {
-  knex.raw(`
-    select t.task_id, task_description, task_deadline, task_status, task_type, task_priority,
-    task_product_manager, task_name, et.engineer_id, engineer_tasks_id, tt.tag_id, tagged_tasks_id,
-    engineer_name, e.label_color as engineer_label_color, tag_name, tg.label_color as tag_label_color
-    from tasks as t
-    left join engineer_tasks as et on t.task_id = et.task_id
-    left join tagged_tasks as tt on t.task_id = tt.task_id
-    left join engineer as e on et.engineer_id = e.engineer_id
-    left join tag as tg on tt.tag_id = tg.tag_id;
-  `)
+  knex.raw(`${getQueryBase};`)
     .then(tasks => {
       const cleanedTasks = []
       tasks.rows.forEach(task => {
@@ -66,17 +68,7 @@ const getTasks = (req, res) => {
 }
 
 const getTask = (req, res) => {
-  knex.raw(`
-    select t.task_id, task_description, task_deadline, task_status, task_type, task_priority,
-    task_product_manager, task_name, et.engineer_id, engineer_tasks_id, tt.tag_id, tagged_tasks_id,
-    engineer_name, e.label_color as engineer_label_color, tag_name, tg.label_color as tag_label_color
-    from tasks as t
-    left join engineer_tasks as et on t.task_id = et.task_id
-    left join tagged_tasks as tt on t.task_id = tt.task_id
-    left join engineer as e on et.engineer_id = e.engineer_id
-    left join tag as tg on tt.tag_id = tg.tag_id
-    where t.task_id=${req.params.id};
-  `)
+  knex.raw(`${getQueryBase} where t.task_id=${req.params.id};`)
     .then(task => {
       const t = task.rows[0]
 
@@ -122,15 +114,6 @@ const getTask = (req, res) => {
     })
 }
 
-// since foreign keys on junction tables are CASCADE deletes, obly have to delete the task
-const deleteTask = async (req, res) => {
-  knex('tasks')
-    .where( { task_id: req.params.id })
-    .delete()
-    .then(task => res.json(task))
-    .catch(err => res.json(err))
-}
-
 const formatTaskBody = async (body) => {
   const statusId = await knex('status').where({ status_name: body.task_status }).then(status => status[0]?.status_id)
   const priorityId = await knex('priority').where({ priority_name: body.task_priority }).then(id => id[0]?.priority_id)
@@ -148,6 +131,48 @@ const formatTaskBody = async (body) => {
   }
 
   return formattedTaskBody
+}
+
+const postTask = async (req, res) => {
+  const formattedTaskBody = await formatTaskBody(req.body)
+
+  knex('tasks')
+    .insert(formattedTaskBody)
+    .returning('task_id')
+    .then(taskId => {
+      req.body.task_tags.forEach(tag => {
+        knex('tag')
+          .where({ tag_name: tag })
+          .then(tag => tag[0]?.tag_id)
+          .then(tagId => {
+            knex('tagged_tasks')
+              .insert({
+                task_id: taskId[0],
+                tag_id: tagId
+              })
+              .then(data => console.log(data))
+          })
+          .then(data => res.json(data))
+      })
+      req.body.task_engineers.forEach(engineer => {
+        knex('engineer')
+          .where({ engineer_name: engineer })
+          .then(engineer => engineer[0]?.engineer_id)
+          .then(engineerId => {
+            knex('engineer_tasks')
+              .insert({
+                task_id: taskId[0],
+                engineer_id: engineerId
+              })
+              .then(data => console.log(data))
+          })
+          .then(data => res.json(data))
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.json(err)
+    })
 }
 
 const updateTask = async (req, res) => {
@@ -211,46 +236,13 @@ const updateTask = async (req, res) => {
     })
 }
 
-const postTask = async (req, res) => {
-  const formattedTaskBody = await formatTaskBody(req.body)
-
+// since foreign keys on junction tables are CASCADE deletes, obly have to delete the task
+const deleteTask = async (req, res) => {
   knex('tasks')
-    .insert(formattedTaskBody)
-    .returning('task_id')
-    .then(taskId => {
-      req.body.task_tags.forEach(tag => {
-        knex('tag')
-          .where({ tag_name: tag })
-          .then(tag => tag[0]?.tag_id)
-          .then(tagId => {
-            knex('tagged_tasks')
-              .insert({
-                task_id: taskId[0],
-                tag_id: tagId
-              })
-              .then(data => console.log(data))
-          })
-          .then(data => res.json(data))
-      })
-      req.body.task_engineers.forEach(engineer => {
-        knex('engineer')
-          .where({ engineer_name: engineer })
-          .then(engineer => engineer[0]?.engineer_id)
-          .then(engineerId => {
-            knex('engineer_tasks')
-              .insert({
-                task_id: taskId[0],
-                engineer_id: engineerId
-              })
-              .then(data => console.log(data))
-          })
-          .then(data => res.json(data))
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.json(err)
-    })
+    .where( { task_id: req.params.id })
+    .delete()
+    .then(task => res.json(task))
+    .catch(err => res.json(err))
 }
 
 module.exports = { getTasks, getTask, postTask, updateTask, deleteTask }
