9902ea8553bdfe9bd40fe203d004b2fa8100df9d
mattdepillis <mattdepillis@gmail.com>
2021-11-30 13:20:49 -0500

    additional changes to knexfile and dockerfile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     client: 'pg',
     // connection: 'postgres://localhost/kanban',
     connection: {
-      host: process.env.HOST || 'host.docker.internal',
+      host: process.env.NODE_ENV === 'development' ? process.env.HOST : 'host.docker.internal',
       port: process.env.DB_PORT || 5432,
       user: process.env.USER || 'postgres',
       password: process.env.PASSWORD,
